c6dbd411342f07e42d595beb20ccaa1115892b72
Vibhu <shihoravibhu@gmail.com>
2025-06-20 21:40:14 +0530

    Update SQL scripts for Lab_2 and Lab_3: drop and truncate tables, add new queries for customer deposits and loan records

--- Lab_Manual/Lab_3.sql
+++ Lab_Manual/Lab_3.sql
@@ -171,3 +171,46 @@ SELECT *
 FROM BORROW 
 WHERE (AMOUNT > 1000 AND AMOUNT <= 7000 ) 
 AND LOANNO BETWEEN 250 AND 600
+
+
+
+-- Part – C: 
+
+-- 1. Display all the detail of customer who deposited more than 5000 without using * from deposit table. 
+
+SELECT ACTNO,CNAME,BNAME,AMOUNT,ADATE
+FROM DEPOSIT 
+WHERE AMOUNT > 5000
+
+-- 2. Retrieve all unique customer names with city. (Use Customer table) 
+
+SELECT DISTINCT CNAME,CITY
+FROM CUSTOMERS 
+
+--3. Retrieve records from the BORROW table where the loan amount is greater than 3000 and the loan number is not a multiple of 3. 
+
+SELECT *
+FROM BORROW
+WHERE AMOUNT>3000 AND (LOANNO % 3 != 0)
+
+--4. Retrieve records from the DEPOSIT table where amount is greater than 2000 also account number is between 100 and 110 and date is after '1-MAR-1995' or before '27-MAR-1996'. 
+
+SELECT *
+FROM DEPOSIT
+WHERE AMOUNT > 2000
+  AND ACTNO BETWEEN 100 AND 110
+  AND (ADATE > '1995-03-01' OR ADATE < '1996-03-27');
+
+-- 5. Retrieve all odd/even value loan number from Borrow table.
+
+-- EVEN
+
+SELECT *
+FROM BORROW
+WHERE LOANNO % 2 = 0
+
+-- ODD
+
+SELECT * 
+FROM BORROW 
+WHERE LOANNO % 2 <> 0
